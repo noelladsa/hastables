504f22de2c5bdc142bff92335820fc51dab59b23
noella <noelladsa@gmail.com>
2015-04-24 13:51:11 -0400

    Fixed updates

--- listoflists.py
+++ listoflists.py
@@ -7,23 +7,38 @@ class ListofLists(object):
 
     def addKeyValuePair(self,key,value):
         listKeyValue = [key,value]
-        self.nativehash.append(listKeyValue)
+        elemArray = self.searchValue(key)
+        if elemArray:
+            elemArray[1] = value
+        else:
+            self.nativehash.append(listKeyValue)
+
+
+    def searchValue(self,key):
+        for item in self.nativehash:
+            if item[0] == key:
+                return item
 
     def getValue(self,key):
         if key is None:
             raise Exception("Key was not set")
 
-        for item in self.nativehash:
-            if item[0] == key:
-                return item[1]
+        element = self.searchValue(key)
+        if element:
+            return element[1]
+        else:
+            raise Exception("Key does not exist")
+
 
     def removeKey(self,key):
         if key is None:
             raise Exception("Key was not set")
 
-        for item in self.nativehash:
-            if item[0] == key:
-                self.nativehash.remove(item)
+        element = self.searchValue(key)
+        if element:
+            self.nativehash.remove(element)
+        else:
+            raise Exception("Key does not exist")
 
     def printHash(self):
         print self.nativehash
@@ -35,6 +50,8 @@ hashTable = ListofLists()
 hashTable.addKeyValuePair("roommate","trash")
 hashTable.addKeyValuePair("recursecenter","largerooms")
 hashTable.printHash()
+
+hashTable.addKeyValuePair("recursecenter","evenlargerooms")
 value = hashTable.getValue('roommate')
 print "The value is",value
 hashTable.removeKey('roommate')
